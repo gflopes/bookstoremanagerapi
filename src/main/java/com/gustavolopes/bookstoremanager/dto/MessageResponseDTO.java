ee5a4444d11b1e3de1560b94af3eafc2cbe6137d
Gustavo Lopes <gflopes22@gmail.com>
2021-01-13 09:32:40 -0300

    Criação das entidades, repository e service

---
+++ src/main/java/com/gustavolopes/bookstoremanager/dto/MessageResponseDTO.java
@@ -0,0 +1,11 @@
+package com.gustavolopes.bookstoremanager.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MessageResponseDTO {
+    
+    public String message;
+}
